8128eca6cbf3255174c1455fefe00d8f2b1db654
Pete Moore <pmoore@mozilla.com>
2020-05-25 20:17:59 +0200

    fix

--- workers/generic-worker/tcmock/factory.go
+++ workers/generic-worker/tcmock/factory.go
@@ -8,46 +8,46 @@ import (
 )
 
 type ServiceFactory struct {
-	t             *testing.T
-	auth          tc.Auth
-	queue         tc.Queue
-	secrets       tc.Secrets
-	purgeCache    tc.PurgeCache
-	workerManager tc.WorkerManager
-	artifacts     tc.Artifacts
+	t               *testing.T
+	TCAuth          tc.Auth
+	TCQueue         tc.Queue
+	TCSecrets       tc.Secrets
+	TCPurgeCache    tc.PurgeCache
+	TCWorkerManager tc.WorkerManager
+	TCArtifacts     tc.Artifacts
 }
 
 func NewServiceFactory(t *testing.T) *ServiceFactory {
 	return &ServiceFactory{
-		auth:          NewAuth(t),
-		queue:         NewQueue(t),
-		secrets:       NewSecrets(t),
-		purgeCache:    NewPurgeCache(t),
-		workerManager: NewWorkerManager(t),
-		artifacts:     NewArtifacts(t),
+		TCAuth:          NewAuth(t),
+		TCQueue:         NewQueue(t),
+		TCSecrets:       NewSecrets(t),
+		TCPurgeCache:    NewPurgeCache(t),
+		TCWorkerManager: NewWorkerManager(t),
+		TCArtifacts:     NewArtifacts(t),
 	}
 }
 
 func (sf *ServiceFactory) Auth(creds *tcclient.Credentials, rootURL string) tc.Auth {
-	return sf.auth
+	return sf.TCAuth
 }
 
 func (sf *ServiceFactory) Queue(creds *tcclient.Credentials, rootURL string) tc.Queue {
-	return sf.queue
+	return sf.TCQueue
 }
 
 func (sf *ServiceFactory) Secrets(creds *tcclient.Credentials, rootURL string) tc.Secrets {
-	return sf.secrets
+	return sf.TCSecrets
 }
 
 func (sf *ServiceFactory) PurgeCache(creds *tcclient.Credentials, rootURL string) tc.PurgeCache {
-	return sf.purgeCache
+	return sf.TCPurgeCache
 }
 
 func (sf *ServiceFactory) WorkerManager(creds *tcclient.Credentials, rootURL string) tc.WorkerManager {
-	return sf.workerManager
+	return sf.TCWorkerManager
 }
 
 func (sf *ServiceFactory) Artifacts(creds *tcclient.Credentials, rootURL string) tc.Artifacts {
-	return sf.artifacts
+	return sf.TCArtifacts
 }
