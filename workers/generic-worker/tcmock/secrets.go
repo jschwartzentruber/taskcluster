685ecf8ccb6ebed80a973882a9526b77c876855a
Pete Moore <pmoore@mozilla.com>
2020-05-26 19:31:22 +0200

    Finished secrets mock

--- workers/generic-worker/tcmock/secrets.go
+++ workers/generic-worker/tcmock/secrets.go
@@ -1,6 +1,7 @@
 package tcmock
 
 import (
+	"encoding/json"
 	"net/http"
 	"testing"
 
@@ -9,13 +10,17 @@ import (
 
 type Secrets struct {
 	// map from secret name to secret value
-	Secrets map[string]tcsecrets.Secret
-	GetFunc func(t *testing.T, w http.ResponseWriter, name string)
+	Secrets    map[string]*tcsecrets.Secret
+	PingFunc   func(t *testing.T, w http.ResponseWriter, req *http.Request)
+	ListFunc   func(t *testing.T, w http.ResponseWriter, req *http.Request)
+	GetFunc    func(secret string, t *testing.T, w http.ResponseWriter, req *http.Request)
+	SetFunc    func(secret string, t *testing.T, w http.ResponseWriter, req *http.Request)
+	RemoveFunc func(secret string, t *testing.T, w http.ResponseWriter, req *http.Request)
 }
 
 func NewSecrets() *Secrets {
 	return &Secrets{
-		Secrets: map[string]tcsecrets.Secret{},
+		Secrets: map[string]*tcsecrets.Secret{},
 		GetFunc: nil,
 	}
 }
@@ -23,49 +28,57 @@ func NewSecrets() *Secrets {
 func (s *Secrets) Handle(handler *http.ServeMux, t *testing.T) {
 
 	const (
-		PathPing    = "/api/secrets/v1/ping"
-		PathSecrets = "/api/secrets/v1/secrets"
-		PathSecret  = "/api/secrets/v1/secret/"
+		PingPath    = "/api/secrets/v1/ping"
+		SecretsPath = "/api/secrets/v1/secrets"
+		SecretPath  = "/api/secrets/v1/secret/"
 	)
 
-	handler.HandleFunc(PathPing, func(w http.ResponseWriter, req *http.Request) {
+	handler.HandleFunc(PingPath, func(w http.ResponseWriter, req *http.Request) {
 		switch req.Method {
 		case "GET":
-			s.Ping(t, w)
+			s.Ping(t, w, req)
 		default:
 			w.WriteHeader(400)
 		}
 	})
 
-	handler.HandleFunc(PathSecrets, func(w http.ResponseWriter, req *http.Request) {
+	handler.HandleFunc(SecretsPath, func(w http.ResponseWriter, req *http.Request) {
 		switch req.Method {
 		case "GET":
-			s.List(t, w)
+			s.List(t, w, req)
 		default:
 			w.WriteHeader(400)
 		}
 	})
 
-	handler.HandleFunc(PathSecret, func(w http.ResponseWriter, req *http.Request) {
-		secretName := req.URL.EscapedPath()[len(PathSecret):]
+	handler.HandleFunc(SecretPath, func(w http.ResponseWriter, req *http.Request) {
+		secret := req.URL.EscapedPath()[len(SecretPath):]
 		switch req.Method {
 		case "GET":
-			s.Get(t, w, secretName)
+			s.Get(secret, t, w, req)
 		case "PUT":
-			s.Set(t, w, secretName, req)
+			s.Set(secret, t, w, req)
 		case "DELETE":
-			s.Remove(t, w, secretName)
+			s.Remove(secret, t, w, req)
 		default:
 			w.WriteHeader(400)
 		}
 	})
 }
 
-func (s *Secrets) Ping(t *testing.T, w http.ResponseWriter) {
+func (s *Secrets) Ping(t *testing.T, w http.ResponseWriter, req *http.Request) {
+	if s.PingFunc != nil {
+		s.PingFunc(t, w, req)
+		return
+	}
 	w.WriteHeader(200)
 }
 
-func (s *Secrets) List(t *testing.T, w http.ResponseWriter) {
+func (s *Secrets) List(t *testing.T, w http.ResponseWriter, req *http.Request) {
+	if s.ListFunc != nil {
+		s.ListFunc(t, w, req)
+		return
+	}
 	names := []string{}
 	for name, _ := range s.Secrets {
 		names = append(names, name)
@@ -76,9 +89,9 @@ func (s *Secrets) List(t *testing.T, w http.ResponseWriter) {
 	WriteAsJSON(t, w, list)
 }
 
-func (s *Secrets) Get(t *testing.T, w http.ResponseWriter, secret string) {
+func (s *Secrets) Get(secret string, t *testing.T, w http.ResponseWriter, req *http.Request) {
 	if s.GetFunc != nil {
-		s.GetFunc(t, w, secret)
+		s.GetFunc(secret, t, w, req)
 		return
 	}
 	if sec, exists := s.Secrets[secret]; exists {
@@ -88,11 +101,29 @@ func (s *Secrets) Get(t *testing.T, w http.ResponseWriter, secret string) {
 	w.WriteHeader(404)
 }
 
-func (s *Secrets) Set(t *testing.T, w http.ResponseWriter, secret string, req *http.Request) {
-	// TODO
+func (s *Secrets) Set(secret string, t *testing.T, w http.ResponseWriter, req *http.Request) {
+	if s.SetFunc != nil {
+		s.SetFunc(secret, t, w, req)
+		return
+	}
+	var sec tcsecrets.Secret
+	dec := json.NewDecoder(req.Body)
+	dec.DisallowUnknownFields()
+	err := dec.Decode(&sec)
+	if err != nil {
+		w.WriteHeader(400)
+		w.Write([]byte(err.Error()))
+		return
+	}
+	s.Secrets[secret] = &sec
+	w.WriteHeader(200)
 }
 
-func (s *Secrets) Remove(t *testing.T, w http.ResponseWriter, secret string) {
+func (s *Secrets) Remove(secret string, t *testing.T, w http.ResponseWriter, req *http.Request) {
+	if s.RemoveFunc != nil {
+		s.RemoveFunc(secret, t, w, req)
+		return
+	}
 	if _, exists := s.Secrets[secret]; exists {
 		delete(s.Secrets, secret)
 		w.WriteHeader(200)
